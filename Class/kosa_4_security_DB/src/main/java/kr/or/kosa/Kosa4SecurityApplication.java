f26499db4de349e6d67ff7c0016a1ac38f15decd
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-06-23 16:43:21 +0900

    today class done

---
+++ Class/kosa_4_security_DB/src/main/java/kr/or/kosa/Kosa4SecurityApplication.java
@@ -0,0 +1,13 @@
+package kr.or.kosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kosa4SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kosa4SecurityApplication.class, args);
+	}
+
+}
